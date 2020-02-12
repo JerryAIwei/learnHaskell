b31828ea0de75b6410dc87de093b340f816fd0bb
1046171559@qq.com <1046171559@qq.com>
2020-02-12 08:21:49 +0800

    recursion

--- Functions.hs
+++ Functions.hs
@@ -6,3 +6,97 @@ sayMe 3 = "Three!"
 sayMe 4 = "Four!"  
 sayMe 5 = "Five!"  
 sayMe x = "Not between 1 and 5" 
+
+factorial :: (Integral a) => a -> a  
+factorial 0 = 1  
+factorial n = n * factorial (n - 1) 
+
+addVectors :: (Num a) => (a, a) -> (a, a) -> (a, a)  
+addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+
+first :: (a, b, c) -> a  
+first (x, _, _) = x  
+  
+second :: (a, b, c) -> b  
+second (_, y, _) = y  
+  
+third :: (a, b, c) -> c  
+third (_, _, z) = z 
+
+head' :: [a] -> a  
+head' [] = error "Can't call head on an empty list, dummy!"  
+head' (x:_) = x
+
+--patterns
+capital :: String -> String  
+capital "" = "Empty string, whoops!"  
+capital all@(x:xs) = "The first letter of " ++ all ++ " is " ++ [x]  
+
+--Guards
+max' :: (Ord a) => a -> a -> a  
+max' a b   
+    | a > b     = a  
+    | otherwise = b  
+
+myCompare :: (Ord a) => a -> a -> Ordering  
+a `myCompare` b  
+    | a > b     = GT  
+    | a == b    = EQ  
+    | otherwise = LT  
+
+-- where
+
+
+bmiTell :: (RealFloat a) => a -> a -> String  
+bmiTell weight height  
+    | bmi <= 18.5 = "You're underweight, you emo, you!"  
+    | bmi <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"  
+    | bmi <= 30.0 = "You're fat! Lose some weight, fatty!"  
+    | otherwise   = "You're a whale, congratulations!"  
+    where bmi = weight / height ^ 2 
+
+
+initials :: String -> String -> String  
+initials firstname lastname = [f] ++ ". " ++ [l] ++ "."  
+    where (f:_) = firstname  
+          (l:_) = lastname 
+          
+--calcBmis :: (RealFloat a) => [(a, a)] -> [a]  
+--calcBmis xs = [bmi w h | (w, h) <- xs]  
+--    where bmi weight height = weight / height ^ 2
+
+--let
+cylinder :: (RealFloat a) => a -> a -> a  
+cylinder r h = 
+    let sideArea = 2 * pi * r * h  
+        topArea = pi * r ^2  
+    in  sideArea + 2 * topArea 
+
+4 * (let a = 9 in a + 1) + 2
+[let square x = x * x in (square 5, square 3, square 2)]
+
+--calcBmis :: (RealFloat a) => [(a, a)] -> [a]  
+--calcBmis xs = [bmi | (w, h) <- xs, let bmi = w / h ^ 2]
+
+calcBmis :: (RealFloat a) => [(a, a)] -> [a]  
+calcBmis xs = [bmi | (w, h) <- xs, let bmi = w / h ^ 2, bmi >= 25.0]
+
+--Case expressions just a syntactic sugar
+head' :: [a] -> a  
+head' xs = case xs of [] -> error "No head for empty lists!"  
+                      (x:_) -> x
+
+--head' :: [a] -> a  
+--head' [] = error "No head for empty lists!"  
+--head' (x:_) = x
+
+describeList :: [a] -> String  
+describeList xs = "The list is " ++ case xs of [] -> "empty."  
+                                               [x] -> "a singleton list."   
+                                               xs -> "a longer list." 
+
+--describeList :: [a] -> String  
+--describeList xs = "The list is " ++ what xs  
+--  where what [] = "empty."  
+--        what [x] = "a singleton list."  
+--        what xs = "a longer list."                                                
